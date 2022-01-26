01527742618003a69ff96b98ce1432219f9d322f
JoHill15 <johill15@heathfieldcc.co.uk>
2022-01-26 09:54:30 +0000

    26/01/22

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-artisturi = input('Choose an artist to see their albums: ')
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+def artistalbums():
+    artisturi = input('Choose an artist to see their albums: ')
+    results = spotify.artist_albums(artisturi, album_type='album')
+    albums = results['items']
+    while results['next']:
+        results = spotify.next(results)
+        albums.extend(results['items'])
 
-results = spotify.artist_albums(artisturi, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
+    for album in albums:
+         print(album['name'])
 
-for album in albums:
-    print(album['name'])
-print("Poo!!")
+
+def artistrelated():
+    artisturi = input('Choose an artist to see their related artists: ')
+    relatedartist = spotify.artist_related_artists(artisturi)
+    print(relatedartist)
+artistalbums()
