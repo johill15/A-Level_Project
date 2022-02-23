7409ec64b517981cc7cbc62464088e880829d1ab
JoHill15 <johill15@heathfieldcc.co.uk>
2022-02-23 09:54:44 +0000

    Fixed

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+#todo: Fill in details of functions
 
 def artistalbums():
+    """
+    shows the albums from the artist that is chosen
+    :return: the albums of the artist
+    """
     artisturi = input('Choose an artist to see their albums: ')
-    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-
     results = spotify.artist_albums(artisturi, album_type='album')
     albums = results['items']
     while results['next']:
@@ -16,7 +20,13 @@ def artistalbums():
 
 
 def artistrelated():
+    """
+    shows the related artists from the chosen artist
+    :return: the related artist
+    """
     artisturi = input('Choose an artist to see their related artists: ')
-    relatedartist = spotipy.artist_related_artists(artisturi)
+    relatedartist = spotify.artist_related_artists(artisturi)
     print(relatedartist)
-artistalbums()
+
+
+artistrelated()
