db7cdd64150c1ef7b61867816a8412f65e832a04
JoHill15 <johill15@heathfieldcc.co.uk>
2022-03-08 14:45:52 +0000

    Added Volume Up and Down and fixed each button to run properly

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import os
 
 music_player = tkr.Tk()
 music_player.title("Music Player")
-music_player.geometry("500x400")
+music_player.geometry("500x550")
 
 directory = askdirectory()
 os.chdir(directory)
 song_list = os.listdir()
 # make a list that only lists possible values (mp3, wav, ogg)
 playlist = tkr.Listbox(music_player, font = "Verdana 12 bold",
-                       fg = "navy", bg = "gold", selectmode = tkr.SINGLE)
+                       fg = "white", bg = "black", selectmode = tkr.SINGLE)
 print (playlist)
 x = 0
 for i in song_list:
@@ -23,31 +23,45 @@ pygame.init()
 pygame.mixer.init()
 
 def play():
-    pygame.mixer.music.load(playlist.get(tkr.ACTIVE))
+    pygame.mixer_music.load(playlist.get(tkr.ACTIVE))
     var.set(playlist.get(tkr.ACTIVE))
-    pygame.mixer.music.play()
+    pygame.mixer_music.play()
 
 def stop():
-    pygame.mixer.music.stop()
+    pygame.mixer_music.stop()
 
 def pause():
-    pygame.mixer.music.pause()
+    pygame.mixer_music.pause()
 
 def unpause():
-    pygame.mixer.music.unpause()
+    pygame.mixer_music.unpause()
+
+def volumeup():
+    pygame.mixer_music.set_volume(1)
+
+def volumedown():
+    pygame.mixer_music.set_volume(0.5)
+
+
 
 Button1 = tkr.Button(music_player, width = 5, height = 3,
                      font = "Verdana 12 bold", text = "PLAY",
-                     command = play, bg = "navy", fg = "gold")
+                     command = play, bg = "grey", fg = "white")
 Button2 = tkr.Button(music_player, width = 5, height = 3,
                      font = "Verdana 12 bold", text = "STOP",
-                     command = play, bg = "navy", fg = "gold")
+                     command = stop, bg = "grey", fg = "white")
 Button3 = tkr.Button(music_player, width = 5, height = 3,
                      font = "Verdana 12 bold", text = "PAUSE",
-                     command = play, bg = "navy", fg = "gold")
+                     command = pause, bg = "grey", fg = "white")
 Button4 = tkr.Button(music_player, width = 5, height = 3,
                      font = "Verdana 12 bold", text = "UNPAUSE",
-                     command = play, bg = "navy", fg = "gold")
+                     command = unpause, bg = "grey", fg = "white")
+Button5 = tkr.Button(music_player, width = 5, height = 3,
+                     font = "Verdana 12 bold", text = "VOLUME UP",
+                     command = volumeup, bg = "grey", fg = "white")
+Button6 = tkr.Button(music_player, width = 5, height = 3,
+                     font = "Verdana 12 bold", text = "VOLUME DOWN",
+                     command = volumedown, bg = "grey", fg = "white")
 
 var = tkr.StringVar()
 song_title = tkr.Label(music_player, font = "Verdana 12 bold", textvariable = var)
@@ -57,6 +71,9 @@ Button1.pack(fill = "x")
 Button2.pack(fill = "x")
 Button3.pack(fill = "x")
 Button4.pack(fill = "x")
+Button5.pack(fill = "x")
+Button6.pack(fill = "x")
 playlist.pack(fill = "both", expand = "yes")
 
 music_player.mainloop()
+
